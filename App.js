346b2041d128ddfdf319dd66609b350be076fa0e
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2024-09-16 12:57:50 -0300

    Adicionando um data com sua KEY ou ID

--- App.js
+++ App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      feed: [
+        {id: 1, nome: 'Guilherme', idade: 28, email: 'GuilhermeMartins@gmail.com'},
+        {id: 2, nome: 'Gustavo', idade: 22, email: 'GustavoMartins@gmail.com'},
+        {id: 3, nome: 'Henrrique', idade: 32, email: 'HenrriqueM@gmail.com'},
+        {id: 4, nome: 'Paulo', idade: 52, email: 'Paulomaria@gmail.com'},
+      ]
+    };
+  }
   render(){
     return(
       <View style={styles.container}>
-        <ScrollView showsVerticalScrollIndicator={false}>
-          <View style={styles.areaBox1}></View>
-          <View style={styles.areaBox2}></View>
-          <View style={styles.areaBox3}></View>
-          <View style={styles.areaBox4}></View>
-        </ScrollView>
+        <FlatList
+        data={this.state.feed}
+        renderItem={ ({item}) => <Pessoa data={item} /> }
+        />
+
       </View>
     );
   }
@@ -20,27 +31,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  areaBox1:{
-    height: 250,
-    backgroundColor: 'red',
-  },
-  areaBox2:{
-    height: 250,
-    backgroundColor: 'blue',
-  },
-  areaBox3:{
-    height: 250,
-    backgroundColor: 'green',
-  },
-  areaBox4:{
-    height: 250,
-    backgroundColor: 'yellow',
-  },
-  centralizar:{
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
 });
 
 export default App;
+
+class Pessoa extends Component{
+  render(){
+    return(
+      <View>
+        <Text> {this.props.data.nome}</Text>
+      </View>
+    );
+  }
+}
